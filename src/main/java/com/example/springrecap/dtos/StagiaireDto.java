ff2be91252aad03bcee43370a82aa8570392607c
healingfields <dihratene@gmail.com>
2022-09-05 19:58:54 +0100

    created the stagiaire DTO

---
+++ src/main/java/com/example/springrecap/dtos/StagiaireDto.java
@@ -0,0 +1,58 @@
+package com.example.springrecap.dtos;
+
+import java.io.Serializable;
+
+public class StagiaireDto implements Serializable {
+
+    private static final long serialVersionUID = -13728473802L;
+
+    private String id;
+
+    private String nom;
+
+    private String prenom;
+
+    private String stagiaireId;
+
+    private GroupeDto groupe;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getStagiaireId() {
+        return stagiaireId;
+    }
+
+    public void setStagiaireId(String stagiaireId) {
+        this.stagiaireId = stagiaireId;
+    }
+
+    public GroupeDto getGroupe() {
+        return groupe;
+    }
+
+    public void setGroupe(GroupeDto groupe) {
+        this.groupe = groupe;
+    }
+}
