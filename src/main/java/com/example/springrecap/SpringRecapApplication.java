769776a6965c5b6925afadbb9105542e98a652a2
healingfields <dihratene@gmail.com>
2022-09-05 13:27:31 +0100

    created the spring application context

--- src/main/java/com/example/springrecap/SpringRecapApplication.java
+++ src/main/java/com/example/springrecap/SpringRecapApplication.java
@@ -2,12 +2,18 @@ package com.example.springrecap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringRecapApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringRecapApplication.class, args);
+    }
+
+    @Bean
+    public SpringApplicationContext springApplicationContext(){
+        return new SpringApplicationContext();
     }
 
 }
